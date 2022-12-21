590a6fd7496b507f3d298d60d25af604ff64b2d4
Sean Jang <seanjang78@gmail.com>
2022-12-21 12:40:51 -0800

    h	u	g	e	 	i	n	i	t	 	c	o	m	m	i	t

---
+++ init.el
@@ -0,0 +1,17 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(magit crux elfeed flycheck google-this marginalia web-mode which-key consult-yasnippet tree-sitter-langs tree-sitter eglot lsp-mode kind-icon corfu neotree general yasnippet helpful doom-modeline all-the-icons doom-themes consult vertico orderless evil use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
