1c8e44f1034b4023a2fad80b0a85ad2ba1dfa4fc
Sean Jang <sylviaviajung@gmail.com>
2022-12-26 11:55:52 -0800

    made another huge batch of changes

--- init.el
+++ init.el
@@ -8,7 +8,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(magit crux elfeed flycheck google-this marginalia web-mode which-key consult-yasnippet tree-sitter-langs tree-sitter eglot lsp-mode kind-icon corfu neotree general yasnippet helpful doom-modeline all-the-icons doom-themes consult vertico orderless evil use-package)))
+	 '(format-all crux elfeed flycheck google-this marginalia web-mode which-key consult-yasnippet tree-sitter-langs tree-sitter eglot lsp-mode kind-icon corfu neotree general yasnippet helpful doom-modeline all-the-icons doom-themes consult vertico orderless evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
